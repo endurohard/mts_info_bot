7171af90c746e1507be2e8822603124e1916a415
Yusup Bagamedov <it0005@ya.ru>
2024-10-17 23:48:37 +0300

    ИсторияВызова

--- functions/transformCallHistory.js
+++ functions/transformCallHistory.js
@@ -5,8 +5,10 @@ export function transformCallHistory(callHistory) {
         const callTime = new Date(call.callTime);
 
         return {
+
             callTime: dateFormat(callTime, "dd.mm.yyyy, h:MM:ss TT"), // Дата и время звонка
             callingNumber: call.callingNumber || 'Неизвестный номер', // Номер звонящего
+            calledNumber: call.calledNumber,   // Номер, на который звонили
             direction: call.direction === 'ORIGINATING' ? 'исходящий' : 'входящий', // Направление звонка
             status: call.status === 'PLACED' ? 'состоявшийся' : 'пропущенный' // Статус звонка
         };
