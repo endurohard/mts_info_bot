fa841ba40dec66991f640edbdf6fcf45b6bb7d7d
Yusup Bagamedov <it0005@ya.ru>
2024-10-18 00:16:55 +0300

    ИсторияВызова2

--- functions/transformCallHistory.js
+++ functions/transformCallHistory.js
@@ -1,25 +1,4 @@
-import dateFormat, { masks } from "dateformat";
-// Функция для преобразования ответа с историей звонков
-export function transformCallHistory(callHistory) {
-    return callHistory.content.map(call => {
-        const callTime = new Date(call.callTime);
-
-        return {
-            enterpriseBwksId: call.enterpriseBwksId,
-            groupBwksId: call.groupBwksId,
-            userId: call.userId,
-            callTime: dateFormat(callTime, "dd.mm.yyyy, h:MM:ss TT"), // Дата и время звонка
-            callingNumber: call.callingNumber,
-            calledNumber: call.calledNumber,
-            duration: call.duration,
-            direction: call.direction === 'ORIGINATING' ? 'исходящий' : 'входящий',
-            status: call.status === 'PLACED' ? 'состоявшийся' : 'пропущенный',
-            answerDuration: call.answerDuration // Добавлено для отображения времени ответа
-        };
-    });
-}
-
-
+import dateFormat from "dateformat";
 
 // Функция для преобразования ответа с историей звонков
 export function transformCallHistory(callHistory) {
@@ -31,6 +10,7 @@ export function transformCallHistory(callHistory) {
             callingNumber: call.callingNumber, // Номер, с которого был сделан звонок
             calledNumber: call.calledNumber,   // Номер, на который звонили
             direction: call.direction === 'ORIGINATING' ? 'исходящий' : 'входящий', // Направление звонка
-            status: call.status === 'PLACED' ? 'состоявшийся' : 'пропущенный', // Статус звонка
+            status: call.status === 'PLACED' ? 'состоявшийся' : 'пропущенный' // Статус звонка
+        };
     });
 }
