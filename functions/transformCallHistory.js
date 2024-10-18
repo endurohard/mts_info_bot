77cd16169d37db24812d561803ced769a13690c3
Yusup Bagamedov <it0005@ya.ru>
2024-10-18 16:34:58 +0300

    Historyjs

--- functions/transformCallHistory.js
+++ functions/transformCallHistory.js
@@ -1,28 +1,16 @@
-import { getCallHistory } from './functions/api.js'; // Импортируем функцию получения истории вызовов
-import { transformCallHistory } from './functions/transformCallHistory.js'; // Импортируем функцию преобразования
+import dateFormat from 'dateformat';
 
-// Обработка нажатия на кнопку 'История вызовов'
-bot.onText(/История вызовов/, async (msg) => {
-    const chatId = msg.chat.id;
-    try {
-        const response = await getCallHistory(); // Получаем историю вызовов
-        const calls = response.content || []; // Проверяем, есть ли вызовы
+// Функция для преобразования ответа с историей звонков
+export function transformCallHistory(callHistory) {
+    return callHistory.content.map(call => {
+        const callTime = new Date(call.callTime);
 
-        if (!Array.isArray(calls) || calls.length === 0) {
-            bot.sendMessage(chatId, 'История вызовов пуста.');
-            return;
-        }
-
-        // Преобразуем и отформатируем историю звонков
-        const transformedCalls = transformCallHistory(response); // Используем transformCallHistory
-
-        const formattedCalls = transformedCalls.map(call => {
-            return `Время звонка: ${call.callTime},\nНомер: ${call.callingNumber},\nНаправление: ${call.direction},\nСтатус: ${call.status}`;
-        }).join('\n\n'); // Соединяем звонки с двумя новыми строками между записями
-
-        bot.sendMessage(chatId, `История вызовов:\n${formattedCalls}`);
-    } catch (error) {
-        console.error('Ошибка при получении истории вызовов:', error.message);
-        bot.sendMessage(chatId, 'Ошибка при получении истории вызовов.');
-    }
-});
+        return {
+            callTime: dateFormat(callTime, "dd.mm.yyyy, h:MM:ss TT"),
+            callingNumber: call.callingNumber,
+            calledNumber: call.calledNumber,
+            direction: call.direction === 'ORIGINATING' ? 'исходящий' : 'входящий',
+            status: call.status === 'PLACED' ? 'состоявшийся' : 'пропущенный'
+        };
+    });
+}
